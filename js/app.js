a372f8f0e04060e62d3d5f9e2734a0a43089e8f5
blair dowis <blairdow@gmail.com>
2016-12-24 00:52:34 -0500

    typed.js added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
-$(function(){
+$(function(){   
     console.log('loaded')
     
+    //side navigation for nav bar on mobile
     $(".button-collapse").sideNav();
+    
+    var waypoint = new Waypoint({
+      element: $('#about-trigger'),
+      handler: function(direction) {
+        if(direction == 'down'){
+            $("#typed").typed({
+                stringsElement: $('#typed-strings'),
+                typeSpeed: 25
+            });
+            this.destroy()
+        }
+      },
+      offset: 50
+    })
 })
